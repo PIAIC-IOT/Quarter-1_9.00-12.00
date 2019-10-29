23fa220489584933d6486bcd94e5e2ba31b04513
unknown <faheemuzzaman@hotmail.com>
2019-10-29 09:58:12 +0500

    Revert "Rust Pre-define Enums, Option with function, If let structure, and Result using Reqwest crate."
    This reverts commit e851cf25220815b366c561598c37632b60d3ad50.

--- class10/target/debug/class10.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Admin\Desktop\Rust\class10\target\debug\class10.pdb: C:\Users\Admin\Desktop\Rust\class10\src\main.rs
--- class10/target/debug/deps/class10-1adc36983fa35760.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Admin\Desktop\Rust\class10\target\debug\deps\class10-1adc36983fa35760.rmeta: src\main.rs
-
-c:\Users\Admin\Desktop\Rust\class10\target\debug\deps\class10-1adc36983fa35760.d: src\main.rs
-
-src\main.rs:
